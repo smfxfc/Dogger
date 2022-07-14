c5effa009c7b254e45acc84aaaeadb85a7a68606
Scott Flynn-Elhoffer <57599533+smfxfc@users.noreply.github.com>
2022-07-13 18:29:44 -0700

    Dogger movement

--- Assets/Scripts/Dogger.cs
+++ Assets/Scripts/Dogger.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Dogger : MonoBehaviour
@@ -5,21 +6,48 @@ public class Dogger : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
-            transform.position += Vector3.up;
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            Move(Vector3.up);
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
-            transform.position += Vector3.down;
+            transform.rotation = Quaternion.Euler(0f,0f, 180f);
+            Move(Vector3.down);
         }
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            transform.position += Vector3.left;
+            Move(Vector3.left);
+            transform.rotation = Quaternion.Euler(0f, 0f, 90f);
         }
         else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
-            transform.position += Vector3.right;
+            transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+            Move(Vector3.right);
         }
     }
 
-    private void Move(Vector3 direction) 
+    private void Move(Vector3 direction)
     {
-        transform.position += direction;    
+        transform.position += direction;
+        Vector3 destination = transform.position + direction;
     }
+    
+    // this block has been commented out because it caused issues with frogger movement along the x/y axis. TODO: fix this so frogger still only moves in increments of 1
+    //    StartCoroutine(Leap(destination));
+    //}
+    //private IEnumerator Leap(Vector3 destination)
+    //{
+    //    Vector3 startPosition = transform.position;
+
+    //    float elapsed = 0f;
+    //    float duration = 0.125f;
+
+    //    while (elapsed < duration)
+    //    {
+    //        float t = elapsed / duration;
+    //        transform.position = Vector3.Lerp(startPosition, destination, t);
+    //        elapsed += Time.deltaTime;
+    //        yield return null;
+    //    }
+      
+    //    // safeguard to assure destination is reached
+    //    transform.position = destination;
+    //}
 }
